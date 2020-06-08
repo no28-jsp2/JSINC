4305d705ae083f8e656a4c06a6baf5bdc18ab8e5
Lim Tae Jung <Lim Tae Jung@DESKTOP-4AIB1B9>
2020-06-08 20:04:58 +0900

    main

--- src/main/java/com/jsinc/controllers/MainController.java
+++ src/main/java/com/jsinc/controllers/MainController.java
@@ -161,13 +161,8 @@ public class MainController {
 	}
 
 	@RequestMapping("registerMem")
-	public String registerMem(MemberDTO dto, Model model, MultipartFile profile, @RequestParam String rank) throws Exception {
+	public String registerMem(MemberDTO dto, Model model, MultipartFile profile) throws Exception {
 
-		if(rank.equals("과장")|| rank.equals("부장")||rank.equals("이사")) {
-			dto.setAuthority("Y");
-		}else {
-			dto.setAuthority("N");
-		}
 		// 업로드
 		UUID uuid = UUID.randomUUID(); // 파일 이름 중복 방지
 		String saveName = uuid + "_" + profile.getOriginalFilename(); // UUID가 붙은 파일이름을 객체에 저장
@@ -182,8 +177,7 @@ public class MainController {
 			return null;
 		}
 		dto.setImg(File.separator + "profile" + File.separator + saveName);
-		
-		
+
 		service = ac.getBean("memberServiceImpl", MemberServiceImpl.class);
 		model.addAttribute("dto", dto);
 		service.execute(model);
